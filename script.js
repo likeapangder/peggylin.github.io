dffb07abba3a7824884d09c784c21dcdcf9dd797
likeapangder <orcapeggy@gmail.com>
2024-09-06 01:23:37 +0800

    updates

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function toggleMenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+
+}
+
+function toggleDropdown() {
+    const dropdown = document.querySelector(".dropdown");
+    dropdown.classList.toggle("open");
 }
